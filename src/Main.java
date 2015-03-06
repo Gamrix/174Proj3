64c863f63013b4b2cca7985cdbe673c15f57dcb9
DannyReinhart <Danreinrm@gmail.com>
2015-03-06 15:16:36 -0800

    initial work on parsing

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+        xmlparse parser = new xmlparse();
+        parser.main();
+    }
+}
--- src/xmlparse.java
+++ src/xmlparse.java
@@ -2,14 +2,13 @@ import java.io.IOException;
 
 import javax.xml.parsers.*;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.io.StringReader;
 
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import sun.tools.java.ClassNotFound;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -18,37 +17,61 @@ public class xmlparse {
     public static void main() {
         try {
 
-
             Connection con = null;
             Statement st = null;
             ResultSet rs = null;
 
-            String url = "jdbc:mysql://localhost:3306/dbname";
-            String user = "";
+            String url = "jdbc:mysql://localhost:3306/healthmessagesexchange";
+            String user = "root";
             String password = "";
 
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(url, user, password);
             st = con.createStatement();
-            rs = st.executeQuery("SELECT * FROM healthmessagesexchange.messagequeue ORDER BY control_id DESC LIMIT 1;");
+            rs = st.executeQuery("SELECT * FROM healthmessagesexchange.messagequeue ORDER BY control_id;");
+            //rs = st.executeQuery("SELECT * FROM healthmessagesexchange.messagequeue ORDER BY control_id LIMIT 1;");
 
+            //the result set is initially before the first row of data. in order to get the first row, call next
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-
             DocumentBuilder db = dbf.newDocumentBuilder();
 
-            Document dom = db.parse(rs.getString(0));
+            while (rs.next()) {
+                printAllColumnNames(rs);
+                System.out.println("parsing item");
+                String test = rs.getString("xmlmessage");
+                System.out.println(test);
+            }
+
 
         } catch (ParserConfigurationException pce) {
             pce.printStackTrace();
-        } catch (SAXException se) {
-            se.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("Caught SQL Exception: " + e.getMessage());
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            System.err.println("Caught ClassNotFound Exception: " + e.getMessage());
+        }
+
+    }
+
+    public static void printAllColumnNames(ResultSet rs) throws SQLException {
+        ResultSetMetaData rsmd = null;
+        try {
+            rsmd = rs.getMetaData();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        int columnCount = 0;
+        try {
+            columnCount = rsmd.getColumnCount();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // The column count starts from 1
+        for (int i = 1; i < columnCount + 1; i++ ) {
+            String name = rsmd.getColumnName(i);
+            System.out.println("column " + i + " is " + name);
+            // Do stuff with name
+        }
     }
 }
-
